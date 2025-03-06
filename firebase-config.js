b67f58eaa52321b5519270a914ca78360baedb5e
Tacothium <hlowden@angelo.edu>
2025-03-06 10:10:49 -0600

    Implementing Firebase - Hailey
    1. implemented firebase authentication
    uses firebase and the login_page.html document to create users accounts and store them.
    
    2. update expense_input.html
    added some scripts to the body that check if a user is logged in before allowing access to the page. I added this on top of the edits previously made, so it should still be up to date.
    
    3. edited the homepage
     changed the button to redirect to the login page instead of the expense_input.html page.
    
    4. added a firebase-config file
    this file is what allows our pages to use firebase

---
+++ firebase-config.js
@@ -0,0 +1,138 @@
+// Your web app's Firebase configuration
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDPYybt1-bo4cNjk2n93TnVJD0RkUV1cgw",
+  authDomain: "test-login-page-6487c.firebaseapp.com",
+  projectId: "test-login-page-6487c",
+  storageBucket: "test-login-page-6487c.firebasestorage.app",
+  messagingSenderId: "998941333585",
+  appId: "1:998941333585:web:34704ab63e7fb8fdc7c070",
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+// Initialize variables
+const auth = firebase.auth();
+const database = firebase.database();
+
+// Set up our register function
+function register() {
+  // Get all our input fields
+  email = document.getElementById("email").value;
+  password = document.getElementById("password").value;
+
+  // Validate input fields
+  if (validate_email(email) == false || validate_password(password) == false) {
+    alert("Email or password is not formatted correctly");
+    return;
+    // Don't continue running the code
+  }
+
+  // Move on with Auth
+  auth
+    .createUserWithEmailAndPassword(email, password)
+    .then(function () {
+      // Declare user variable
+      var user = auth.currentUser;
+
+      // Add this user to Firebase Database
+      var database_ref = database.ref();
+
+      // Create User data
+      var user_data = {
+        email: email,
+        last_login: Date.now(),
+      };
+
+      // Push to Firebase Database
+      database_ref.child("users/" + user.uid).set(user_data);
+
+      // DOne
+      alert("User created successfully");
+    })
+    .catch(function (error) {
+      // Firebase will use this to alert of its errors
+      var error_code = error.code;
+      var error_message = error.message;
+
+      alert(error_message);
+    });
+}
+
+// Set up our login function
+function login() {
+  // Get all our input fields
+  email = document.getElementById("email").value;
+  password = document.getElementById("password").value;
+
+  // Validate input fields
+  if (validate_email(email) == false || validate_password(password) == false) {
+    alert("Email or password is incorrect");
+    return;
+    // Don't continue running the code
+  }
+
+  auth
+    .signInWithEmailAndPassword(email, password)
+    .then(function () {
+      // Declare user variable
+      var user = auth.currentUser;
+
+      // Add this user to Firebase Database
+      var database_ref = database.ref();
+
+      // Create User data
+      var user_data = {
+        last_login: Date.now(),
+      };
+
+      // Push to Firebase Database
+      database_ref.child("users/" + user.uid).update(user_data);
+
+      // Done
+      alert("User logged in");
+
+      // Redirect users to the finance dashboard
+      window.location.href = "/Subpages/expense_input.html";
+    })
+    .catch(function (error) {
+      // Firebase will use this to alert of its errors
+      var error_code = error.code;
+      var error_message = error.message;
+
+      alert(error_message);
+    });
+}
+
+// Validate Functions
+function validate_email(email) {
+  expression = /^[^@]+@\w+(\.\w+)+\w$/;
+  if (expression.test(email) == true) {
+    // Email is good
+    return true;
+  } else {
+    // Email is not good
+    return false;
+  }
+}
+
+function validate_password(password) {
+  // Firebase only accepts lengths greater than 6 and less than 26
+  if (password <= 6 || password > 25) {
+    return false;
+  } else {
+    return true;
+  }
+}
+
+function validate_field(field) {
+  if (field == null) {
+    return false;
+  }
+
+  if (field.length <= 0) {
+    return false;
+  } else {
+    return true;
+  }
+}
